cc352fd9f43bb6306d73c6be109a042941f3c355
Patrick Stapfer <ps@spp.at>
2015-02-10 17:56:26 +0100

    Fixed prototype inheritance problem

--- udev-ext-media.js
+++ udev-ext-media.js
@@ -3,7 +3,10 @@ var util = require("util");
 
 var EventEmitter = require("events").EventEmitter;
 
-util.inherits(udev.Monitor, EventEmitter);
+//This method overwrites the close-method, this is not ideal...
+//going back to the "old" inheritance method
+//util.inherits(udev.Monitor, EventEmitter);
+udev.Monitor.prototype.__proto__ = EventEmitter.prototype;
 
 module.exports = {
     monitor: function() { return new udev.Monitor(); },
